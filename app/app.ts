ef3adb3b87bc444c35827688e778336e3df1a5b3
Sean Watkins <slwatkins@uh.edu>
2016-09-25 05:25:36 -0500

    Correct Typings

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,4 @@
-/// <reference path="../typings/electron.d.ts" />
-/// <reference path="../typings/csv-parse.d.ts" />
-/// <reference path="../typings/csv-stringify" />
-/// <reference path="../typings/mime.d.ts" />
-/// <reference path="../typings/object-hash.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/csv-parse/index.d.ts" />
+/// <reference path="globals/csv-stringify/index.d.ts" />
+/// <reference path="globals/github-electron/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/object-hash/index.d.ts" />
+/// <reference path="modules/mime/index.d.ts" />
