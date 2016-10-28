b9302f25e4ce7560b64ffef60ca7715f0007c005
Sean Watkins <slwatkins@uh.edu>
2016-10-28 09:54:21 -0500

    Added repeatable fields to autofill. Closes #16

--- app/digital-objects/autofill.component.ts
+++ app/digital-objects/autofill.component.ts
@@ -1,5 +1,5 @@
-import { OnInit, Component, HostListener, Output, EventEmitter, ViewChild } from '@angular/core';
-
+import { OnInit, Component, HostListener, Output, EventEmitter } from '@angular/core';
+import { ViewChild, ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 import { VocabularyAutocomplete } from '../shared/vocabulary-autocomplete.directive';
 
 import { ObjectService } from '../shared/object.service';
@@ -15,16 +15,19 @@ import { MapField } from '../shared/map-field';
     VocabularyAutocomplete
   ]
 })
-export class AutofillComponent implements OnInit {
+export class AutofillComponent implements OnInit, AfterViewInit {
 
   selectedField: string;
   selectedRange: any;
   fieldValue: string;
+  fieldValues: any[];
   fields: MapField[];
+  fieldRepeatable: boolean;
 
   @Output() dismissAutofill = new EventEmitter();
 
   @ViewChild('fieldInput') fieldInput;
+  @ViewChildren('fieldRepeatableInput') fieldRepeatableInput: QueryList<ElementRef>;
 
   constructor(
     private objectService: ObjectService,
@@ -33,6 +36,19 @@ export class AutofillComponent implements OnInit {
 
   ngOnInit(): void {
     this.fields = this.map.getMapFields();
+    this.fieldRepeatable = false;
+    this.fieldValues = [];
+  }
+
+  ngAfterViewInit(): void {
+    this.fieldRepeatableInput.changes.subscribe(children => {
+      if (this.fieldRepeatable) {
+        children.last.nativeElement.focus();
+      }
+      else {
+        this.focusInputField();
+      }
+    });
   }
 
   focusInputField(): void {
@@ -41,6 +57,10 @@ export class AutofillComponent implements OnInit {
 
   autofillObjects(): void {
     if (this.selectedField) {
+      if (this.fieldRepeatable) {
+        this.fieldValue = this.fieldValues
+          .map(val => { return val.value; }).join(';');
+      }
       this.objectService.autofill(this.selectedField, this.fieldValue);
       this.objectService.saveObjects();
     }
@@ -48,15 +68,25 @@ export class AutofillComponent implements OnInit {
   }
 
   onFieldChange(value: string): void {
-    this.focusInputField();
-
     this.selectedField = value;
+    this.fieldValues = [{value: ''}];
+
     let field = this.map.getMapFieldByFullName(this.selectedField);
     this.selectedRange = field.range;
+    this.fieldRepeatable = field.repeatable;
+    this.focusInputField();
   }
 
   close(): void {
     this.dismissAutofill.emit();
+  }
+
+  addRepeatable(index: number = 0): void {
+    this.fieldValues.splice(index + 1, 0, {'value': ''});
+  }
+
+  removeRepeatable(index: number): void {
+    this.fieldValues.splice(index, 1);
   }
 
   @HostListener('document:keydown', ['$event']) onKeypress(e) {
