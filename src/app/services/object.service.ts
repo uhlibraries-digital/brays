52aedf73fc197098c8539f0c24ceb54c3cb54b10
Sean Watkins <slwatkins@uh.edu>
2018-02-05 14:10:34 -0600

    Update Dates when project file changes

--- src/app/services/object.service.ts
+++ src/app/services/object.service.ts
@@ -321,6 +321,9 @@ export class ObjectService {
     object.originalData = pObject;
     object.id = index;
     object.title = this.padLeft(index, 3, '0') + ': ' + object.getFieldValue('dcterms.title');
+    if (object.getFieldValue('dc.date') === '' && pObject.dates) {
+      object.setField('dc.date', pObject.dates.join('; '));
+    }
     object.setField('dcterms.source', pObject.pm_ark || '');
     object.setField('uhlib.aSpaceUri', ((pObject.artificial ? pObject.parent_uri : pObject.record_uri) || ''));
     object.productionNotes = pObject.productionNotes || '';
