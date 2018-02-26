d741816bd0c0b53a989b23d302e29b0870c563fe
Sean Watkins <slwatkins@uh.edu>
2018-02-26 12:27:51 -0600

    Fix padding problem

--- src/app/classes/object.ts
+++ src/app/classes/object.ts
@@ -54,6 +54,7 @@ export class Object{
 
   private padLeft(value: any, length: number, character: string): string {
     value = String(value);
+    if (value.length > length) { return value; }
     return Array(length - value.length + 1).join(character || " ") + value;
   }
 
